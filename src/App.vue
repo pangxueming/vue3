2311565264b0521be2ff767de7f69c9984d7ded5
pangxueming <1492678698@qq.com>
2022-05-24 18:06:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const counter = reactive({
+  count: 1
+});
+const message = ref('hello world!');
+
+counter.count++;
+
+
+
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+  <h3>当前计算： {{ counter.count }}</h3>
+  <button @click="counter.count++">+1</button>
+</template>
+
+<style>
+</style>
